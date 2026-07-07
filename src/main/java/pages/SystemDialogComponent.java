733295380341d08f4ba9425b04b0255268f72782
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2026-07-07 17:38:29 +0300

    Add SystemDialogComponent for crash or freeze

---
+++ src/main/java/pages/SystemDialogComponent.java
@@ -0,0 +1,88 @@
+package pages;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.InteractsWithApps;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class SystemDialogComponent {
+    private static final String APP_PACKAGE = "com.saucelabs.mydemoapp.android";
+
+    private final AppiumDriver driver;
+    private final By systemDialogLocator = AppiumBy.id("android:id/parentPanel");
+
+    // Crash dialog locators
+    private final By crashDialogTextLocator = AppiumBy.id("android:id/alertTitle");
+    private final By crashCloseButtonLocator = AppiumBy.id("android:id/aerr_close");
+    private final By crashAppInfoButtonLocator = AppiumBy.id("android:id/aerr_app_info");
+
+    // ANR dialog locators (App isn’t responding)
+    private final By anrMessageLocator = AppiumBy.id("android:id/message");
+    private final By anrCloseButtonLocator = AppiumBy.id("android:id/button1");
+    private final By anrWaitButtonLocator = AppiumBy.id("android:id/button2");
+
+    private WebElement crashDialogText() {
+        return driver.findElement(crashDialogTextLocator);
+    } // My Demo App keeps stopping
+
+    private WebElement crashCloseButton() {
+        return driver.findElement(crashCloseButtonLocator);
+    }
+
+    private WebElement anrDialogText() {
+        return driver.findElement(anrMessageLocator);
+    } // App isn’t responding
+
+    private WebElement anrCloseButton() {
+        return driver.findElement(anrCloseButtonLocator);
+    }
+
+    private WebElement anrWaitButton() {
+        return driver.findElement(anrWaitButtonLocator);
+    }
+
+    public SystemDialogComponent(AppiumDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isCrashDialogVisible() {
+        return !driver.findElements(systemDialogLocator).isEmpty()
+                && crashDialogText().isDisplayed()
+                && crashDialogText().getText().contains("keeps stopping");
+    }
+
+    public boolean isAnrDialogVisible() {
+        return !driver.findElements(systemDialogLocator).isEmpty()
+                && anrDialogText().isDisplayed()
+                && anrDialogText().getText().contains("isn’t responding");
+    }
+
+    public void handleDialog() {
+        if (isCrashDialogVisible()) {
+            System.out.println("Crash dialog detected.");
+            crashCloseButton().click();
+
+            // Restart app after crash
+            restartApp();
+
+            return;
+        }
+
+        if (isAnrDialogVisible()) {
+            System.out.println("ANR dialog detected.");
+            // Prefer Wait
+            if (!driver.findElements(anrWaitButtonLocator).isEmpty()) {
+                anrWaitButton().click(); // Let the app recover
+            } else {
+                anrCloseButton().click(); // Android decided to kill it
+                restartApp();
+            }
+        }
+    }
+
+    private void restartApp() {
+        ((InteractsWithApps) driver).terminateApp(APP_PACKAGE);
+        ((InteractsWithApps) driver).activateApp(APP_PACKAGE);
+    }
+}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,21 +6,27 @@ import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import pages.SystemDialogComponent;
 import utils.ScreenshotOnFailureExtension;
 import utils.ScreenshotUtils;
 
 @ExtendWith({ScreenshotOnFailureExtension.class, AllureJunit5.class})
 public abstract class BaseTest {
     protected AppiumDriver driver;
+    private SystemDialogComponent systemDialog;
 
     @BeforeEach
     public void setUp() {
         driver = DriverFactory.getDriver();
         ScreenshotUtils.setDriver(driver);
+        systemDialog = new SystemDialogComponent(driver);
+
+        systemDialog.handleDialog();
     }
 
     @AfterEach
     public void tearDown() {
+        systemDialog.handleDialog();
         DriverFactory.closeDriver();
     }
 }
